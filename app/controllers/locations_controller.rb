c62f9e0a47bc352893bdf53ba4fc37d406e1ed96
Daniel Rooker <dannyrooks@gmail.com>
2020-02-04 20:45:43 -0500

    adds methods to location controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,7 +1,15 @@
 class LocationController < ApplicationController
+  
+  def index
+    @locations = Location.all
+  end
+  
   def new
+    @location = Location.new
   end
 
   def create
   end
+
+
 end
